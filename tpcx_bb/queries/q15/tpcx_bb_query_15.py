eff7c87ae90239e5c3beea3c74e43d7c1d0d00a2
Nick Becker <nickb500@gmail.com>
2020-06-26 13:02:07 -0700

    q15 and q16 dask

--- tpcx_bb/queries/q15/tpcx_bb_query_15.py
+++ tpcx_bb/queries/q15/tpcx_bb_query_15.py
@@ -22,6 +22,7 @@ from xbb_tools.utils import (
     benchmark,
     tpcxbb_argparser,
     run_dask_cudf_query,
+    convert_datestring_to_days,
 )
 from xbb_tools.readers import build_reader
 
@@ -40,19 +41,16 @@ date_cols = ["d_date", "d_date_sk"]
 item_cols = ["i_item_sk", "i_category_id"]
 
 
-### Util Functions
-def convert_datestring_to_datetime(df, date_col="d_date", date_format="%Y-%m-%d"):
-    datetime_array = cudf.core.column.column_empty(len(df), dtype=np.int64)
-    df[date_col].str.timestamp2int(
-        format=date_format, units="D", devptr=datetime_array.data_ptr
-    )
-    df[date_col] = datetime_array
-    return df
-
-
-@benchmark(dask_profile=cli_args["dask_profile"])
+@benchmark(
+    compute_result=cli_args["get_read_time"], dask_profile=cli_args["dask_profile"]
+)
 def read_tables():
-    table_reader = build_reader(basepath=cli_args["data_dir"])
+    table_reader = build_reader(
+        cli_args["file_format"],
+        basepath=cli_args["data_dir"],
+        repartition_small_table=cli_args["repartition_small_table"],
+        split_row_groups=cli_args["split_row_groups"],
+    )
 
     store_sales_df = table_reader.read("store_sales", relevant_cols=store_sales_cols)
     date_dim_df = table_reader.read("date_dim", relevant_cols=date_cols)
@@ -61,7 +59,9 @@ def read_tables():
     return store_sales_df, date_dim_df, item_df
 
 
-@benchmark(dask_profile=cli_args["dask_profile"])
+@benchmark(
+    compute_result=cli_args["get_read_time"], dask_profile=cli_args["dask_profile"]
+)
 def main(client):
 
     store_sales_df, date_dim_df, item_df = read_tables()
@@ -70,7 +70,7 @@ def main(client):
     store_sales_df = store_sales_df.query(f"ss_store_sk == {q15_store_sk}")
 
     ### Query 1. Date Time Filteration Logic
-    date_dim_cov_df = date_dim_df.map_partitions(convert_datestring_to_datetime)
+    date_dim_cov_df = date_dim_df.map_partitions(convert_datestring_to_days)
 
     q15_start_dt = datetime.datetime.strptime(q15_startDate, "%Y-%m-%d")
     q15_start_dt = (q15_start_dt - datetime.datetime(1970, 1, 1)) / datetime.timedelta(
