9af14cfd89c82ca1acc1662e9195fe043842cd87
Nick Becker <nickb500@gmail.com>
2020-07-02 13:39:44 -0700

    update TODO in q04 for reset_index workaround

--- tpcx_bb/queries/q04/tpcx_bb_query_04.py
+++ tpcx_bb/queries/q04/tpcx_bb_query_04.py
@@ -59,7 +59,7 @@ def read_tables():
 def abandonedShoppingCarts(df, DYNAMIC_CAT_CODE, ORDER_CAT_CODE):
     import cudf
 
-    # work around for https://github.com/rapidsai/cudf/issues/5470
+    # TODO: test without reset index
     df.reset_index(drop=True, inplace=True)
 
     # Select groups where last dynamic row comes after last order row
@@ -67,7 +67,7 @@ def abandonedShoppingCarts(df, DYNAMIC_CAT_CODE, ORDER_CAT_CODE):
         (df["wp_type_codes"] == ORDER_CAT_CODE)
         | (df["wp_type_codes"] == DYNAMIC_CAT_CODE)
     ]
-    # work around for https://github.com/rapidsai/cudf/issues/5470
+    # TODO: test without reset index
     filtered_df.reset_index(drop=True, inplace=True)
     # Create a new column that is the concatenation of timestamp and wp_type_codes
     # (eg:123456:3, 234567:5)
