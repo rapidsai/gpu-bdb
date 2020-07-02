0980a9a41f342b3f70a601bdad48f623caf25fba
Nick Becker <nickb500@gmail.com>
2020-07-02 13:27:13 -0700

    update benchmark runner for repeats and garbage collection

--- tpcx_bb/benchmark_runner.py
+++ tpcx_bb/benchmark_runner.py
@@ -4,6 +4,9 @@ os.environ["tpcxbb_benchmark_sweep_run"] = "True"
 import time
 
 qnums = [str(i).zfill(2) for i in range(1, 31)]
+n_repeats = 3
+
+
 if __name__ == "__main__":
     from xbb_tools.cluster_startup import attach_to_cluster, import_query_libs
     from xbb_tools.utils import run_dask_cudf_query, tpcxbb_argparser
@@ -12,9 +15,7 @@ if __name__ == "__main__":
     import_query_libs()
 
     q_func_d = {
-        qnum: importlib.import_module(
-            f"queries.q{qnum}.tpcx_bb_query_{qnum}"
-        ).main
+        qnum: importlib.import_module(f"queries.q{qnum}.tpcx_bb_query_{qnum}").main
         for qnum in qnums
     }
 
@@ -35,7 +36,9 @@ if __name__ == "__main__":
         with open("current_query_num.txt", "w") as fp:
             fp.write(qnum)
 
-        run_dask_cudf_query(
-            cli_args=cli_args, client=client, query_func=q_func
-        )
-        time.sleep(3)
+        for r in range(n_repeats):
+            run_dask_cudf_query(cli_args=cli_args, client=client, query_func=q_func)
+            print(client.run(gc.collect))
+            print(client.run_on_scheduler(gc.collect))
+            print(gc.collect())
+            time.sleep(3)
