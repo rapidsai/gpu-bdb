2282d4f6f9b1c74726be8d6e1ad82c8a7b4cecb5
christian <ccce91@gmail.com>
2020-07-16 19:35:25 -0500

    Updating q19

--- tpcx_bb/queries/q19/tpcx_bb_query_19_sql.py
+++ tpcx_bb/queries/q19/tpcx_bb_query_19_sql.py
@@ -17,14 +17,13 @@
 
 import sys
 
-from blazingsql import BlazingContext
 from xbb_tools.cluster_startup import attach_to_cluster
 import os
 
 from xbb_tools.utils import (
     benchmark,
     tpcxbb_argparser,
-    run_bsql_query,
+    run_query,
 )
 
 from xbb_tools.text import (
@@ -41,19 +40,15 @@ q19_returns_dates_IN = ["2004-03-08", "2004-08-02", "2004-11-15", "2004-12-20"]
 eol_char = "è"
 
 
-@benchmark(
-    compute_result=cli_args["get_read_time"], dask_profile=cli_args["dask_profile"]
-)
-def read_tables(data_dir):
+def read_tables(data_dir, bc):
     bc.create_table('web_returns', data_dir + "web_returns/*.parquet")
     bc.create_table('date_dim', data_dir + "date_dim/*.parquet")
     bc.create_table('product_reviews', data_dir + "product_reviews/*.parquet")
     bc.create_table('store_returns', data_dir + "store_returns/*.parquet")
 
 
-@benchmark(dask_profile=cli_args["dask_profile"])
-def main(data_dir, client):
-    read_tables(data_dir)
+def main(data_dir, client, bc, config):
+    benchmark(read_tables, data_dir, bc, dask_profile=config["dask_profile"])
 
     query = f"""
         WITH dateFilter AS
@@ -164,14 +159,6 @@ def main(data_dir, client):
 
 
 if __name__ == "__main__":
-    client = attach_to_cluster(cli_args)
-
-    bc = BlazingContext(
-        dask_client=client,
-        pool=True,
-        network_interface=os.environ.get("INTERFACE", "eth0"),
-    )
-
-    run_bsql_query(
-        cli_args=cli_args, client=client, query_func=main
-    )
+    config = tpcxbb_argparser()
+    client, bc = attach_to_cluster(config, create_blazing_context=True)
+    run_query(config=config, client=client, query_func=main, blazing_context=bc)
