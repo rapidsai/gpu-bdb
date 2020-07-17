bb1ce6be23a2ab0dc46fa3c1c7d277c8e2509fbd
christian <ccce91@gmail.com>
2020-07-16 21:06:11 -0500

    updating q29

--- tpcx_bb/queries/q29/tpcx_bb_query_29_sql.py
+++ tpcx_bb/queries/q29/tpcx_bb_query_29_sql.py
@@ -17,14 +17,12 @@
 
 import sys
 
-from blazingsql import BlazingContext
 from xbb_tools.cluster_startup import attach_to_cluster
-import os
 
 from xbb_tools.utils import (
     benchmark,
     tpcxbb_argparser,
-    run_bsql_query,
+    run_query,
 )
 
 cli_args = tpcxbb_argparser()
@@ -34,17 +32,14 @@ cli_args = tpcxbb_argparser()
 q29_limit = 100
 
 
-@benchmark(
-    compute_result=cli_args["get_read_time"], dask_profile=cli_args["dask_profile"]
-)
-def read_tables(data_dir):
+def read_tables(data_dir, bc):
     bc.create_table('item', data_dir + "item/*.parquet")
     bc.create_table('web_sales', data_dir + "web_sales/*.parquet")
 
 
-@benchmark(dask_profile=cli_args["dask_profile"])
-def main(data_dir, client):
-    read_tables(data_dir)
+def main(data_dir, client, bc, config):
+    benchmark(read_tables, data_dir, bc, dask_profile=config["dask_profile"])
+
 
     query_distinct = """
         SELECT DISTINCT i_category_id, ws_order_number
@@ -52,7 +47,6 @@ def main(data_dir, client):
         WHERE ws.ws_item_sk = i.i_item_sk
         AND i.i_category_id IS NOT NULL
     """
-
     result_distinct = bc.sql(query_distinct)
 
     bc.create_table('distinct_table', result_distinct)
@@ -77,14 +71,6 @@ def main(data_dir, client):
 
 
 if __name__ == "__main__":
-    client = attach_to_cluster(cli_args)
-
-    bc = BlazingContext(
-        dask_client=client,
-        pool=True,
-        network_interface=os.environ.get("INTERFACE", "eth0"),
-    )
-
-    run_bsql_query(
-        cli_args=cli_args, client=client, query_func=main
-    )
+    config = tpcxbb_argparser()
+    client, bc = attach_to_cluster(config, create_blazing_context=True)
+    run_query(config=config, client=client, query_func=main, blazing_context=bc)
