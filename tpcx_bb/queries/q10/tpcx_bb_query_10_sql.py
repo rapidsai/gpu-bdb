23c5b2e8ec813a291d31775bfc5058ccb1aa6252
christian <ccce91@gmail.com>
2020-07-16 19:09:43 -0500

    Updating q10

--- tpcx_bb/queries/q10/tpcx_bb_query_10_sql.py
+++ tpcx_bb/queries/q10/tpcx_bb_query_10_sql.py
@@ -17,14 +17,13 @@
 
 import sys
 
-from blazingsql import BlazingContext
 from xbb_tools.cluster_startup import attach_to_cluster
 import os
 
 from xbb_tools.utils import (
     benchmark,
     tpcxbb_argparser,
-    run_bsql_query,
+    run_query,
 )
 
 from xbb_tools.text import (
@@ -37,16 +36,12 @@ cli_args = tpcxbb_argparser()
 eol_char = "è"
 
 
-@benchmark(
-    compute_result=cli_args["get_read_time"], dask_profile=cli_args["dask_profile"]
-)
-def read_tables(data_dir):
+def read_tables(data_dir, bc):
     bc.create_table('product_reviews', data_dir + "product_reviews/*.parquet")
 
 
-@benchmark(dask_profile=cli_args["dask_profile"])
-def main(data_dir, client):
-    read_tables(data_dir)
+def main(data_dir, client, bc, config):
+    benchmark(read_tables, data_dir, bc, dask_profile=config["dask_profile"])
 
     query_1 = """
         SELECT pr_item_sk,
@@ -136,14 +131,6 @@ def main(data_dir, client):
 
 
 if __name__ == "__main__":
-    client = attach_to_cluster(cli_args)
-
-    bc = BlazingContext(
-        dask_client=client,
-        pool=True,
-        network_interface=os.environ.get("INTERFACE", "eth0"),
-    )
-
-    run_bsql_query(
-        cli_args=cli_args, client=client, query_func=main
-    )
+    config = tpcxbb_argparser()
+    client, bc = attach_to_cluster(config, create_blazing_context=True)
+    run_query(config=config, client=client, query_func=main, blazing_context=bc)
