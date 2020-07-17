57af37365497aab45d15596febbf36e58c922449
christian <ccce91@gmail.com>
2020-07-16 21:30:48 -0500

    updating q08

--- tpcx_bb/queries/q08/tpcx_bb_query_08_sql.py
+++ tpcx_bb/queries/q08/tpcx_bb_query_08_sql.py
@@ -17,9 +17,7 @@
 
 import sys
 
-from blazingsql import BlazingContext
 from xbb_tools.cluster_startup import attach_to_cluster
-import os
 import cupy as cp
 import numpy as np
 import cudf
@@ -27,7 +25,7 @@ import cudf
 from xbb_tools.utils import (
     benchmark,
     tpcxbb_argparser,
-    run_bsql_query,
+    run_query,
 )
 
 cli_args = tpcxbb_argparser()
@@ -142,10 +140,7 @@ def prep_for_sessionization(df, review_cat_code):
     return df_filtered
 
 
-@benchmark(
-    compute_result=cli_args["get_read_time"], dask_profile=cli_args["dask_profile"]
-)
-def read_tables(data_dir):
+def read_tables(data_dir, bc):
     bc.create_table("web_clickstreams", data_dir + "/web_clickstreams/*.parquet")
     bc.create_table("web_sales", data_dir + "/web_sales/*.parquet")
     bc.create_table("web_page", data_dir + "/web_page/*.parquet")
@@ -155,9 +150,8 @@ def read_tables(data_dir):
     bc.create_table("warehouse", data_dir + "/warehouse/*.parquet")
 
 
-@benchmark(dask_profile=cli_args["dask_profile"])
-def main(data_dir, client):
-    read_tables(data_dir)
+def main(data_dir, client, bc, config):
+    benchmark(read_tables, data_dir, bc, dask_profile=config["dask_profile"])
 
     query_1 = f"""
         SELECT d_date_sk
@@ -249,14 +243,6 @@ def main(data_dir, client):
 
 
 if __name__ == "__main__":
-    client = attach_to_cluster(cli_args)
-
-    bc = BlazingContext(
-        dask_client=client,
-        pool=True,
-        network_interface=os.environ.get("INTERFACE", "eth0"),
-    )
-
-    run_bsql_query(
-        cli_args=cli_args, client=client, query_func=main
-    )
+    config = tpcxbb_argparser()
+    client, bc = attach_to_cluster(config, create_blazing_context=True)
+    run_query(config=config, client=client, query_func=main, blazing_context=bc)
