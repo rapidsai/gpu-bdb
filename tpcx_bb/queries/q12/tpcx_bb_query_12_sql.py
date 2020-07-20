3a750248e5dfba241bf45fb7e2b08b1d6edd859a
christian <ccce91@gmail.com>
2020-07-20 13:41:16 -0500

    Updating q12

--- tpcx_bb/queries/q12/tpcx_bb_query_12_sql.py
+++ tpcx_bb/queries/q12/tpcx_bb_query_12_sql.py
@@ -17,36 +17,28 @@
 
 import sys
 
-from blazingsql import BlazingContext
 from xbb_tools.cluster_startup import attach_to_cluster
-import os
 
 from xbb_tools.utils import (
     benchmark,
     tpcxbb_argparser,
-    run_bsql_query,
+    run_query,
 )
-
-cli_args = tpcxbb_argparser()
 
 
 # -------- Q12 -----------
 q12_i_category_IN = "'Books', 'Electronics'"
 
 
-@benchmark(
-    compute_result=cli_args["get_read_time"], dask_profile=cli_args["dask_profile"]
-)
-def read_tables(data_dir):
+def read_tables(data_dir, bc):
     bc.create_table("web_clickstreams",
                     data_dir + "/web_clickstreams/*.parquet")
     bc.create_table("store_sales", data_dir + "/store_sales/*.parquet")
     bc.create_table("item", data_dir + "/item/*.parquet")
 
 
-@benchmark(dask_profile=cli_args["dask_profile"])
-def main(data_dir, client):
-    read_tables(data_dir)
+def main(data_dir, client, bc, config):
+    benchmark(read_tables, data_dir, bc, dask_profile=config["dask_profile"])
 
     query = f"""
         SELECT DISTINCT wcs_user_sk
@@ -81,14 +73,6 @@ def main(data_dir, client):
 
 
 if __name__ == "__main__":
-    client = attach_to_cluster(cli_args)
-
-    bc = BlazingContext(
-        dask_client=client,
-        pool=True,
-        network_interface=os.environ.get("INTERFACE", "eth0"),
-    )
-
-    run_bsql_query(
-        cli_args=cli_args, client=client, query_func=main
-    )
+    config = tpcxbb_argparser()
+    client, bc = attach_to_cluster(config, create_blazing_context=True)
+    run_query(config=config, client=client, query_func=main, blazing_context=bc)
