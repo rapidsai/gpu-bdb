3ed4d0d4ba238622d8c02e5f19790a099cf46721
christian <ccce91@gmail.com>
2020-08-24 23:14:19 -0500

    Apply CAST to the first SQL

--- tpcx_bb/queries/q02/tpcx_bb_query_02_sql.py
+++ tpcx_bb/queries/q02/tpcx_bb_query_02_sql.py
@@ -44,8 +44,8 @@ def main(data_dir, client, bc, config):
 
     query_1 = """
         SELECT
-            wcs_user_sk,
-            wcs_item_sk,
+            CAST( wcs_user_sk AS INTEGER) AS wcs_user_sk,
+            CAST( wcs_item_sk AS INTEGER) AS wcs_item_sk,
             (wcs_click_date_sk * 86400 + wcs_click_time_sk) AS tstamp_inSec
         FROM web_clickstreams
         WHERE wcs_item_sk IS NOT NULL
