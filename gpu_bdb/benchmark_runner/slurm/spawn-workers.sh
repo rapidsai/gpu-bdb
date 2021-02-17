d4e7de3d11840406c9d646c7207da7ab36ff7c8b
Nick Becker <nickb500@gmail.com>
2021-02-17 11:11:40 -0500

    default to backfill, name the worker job, and sleep in spawn workers to handle simultaneous allocation

--- gpu_bdb/benchmark_runner/slurm/spawn-workers.sh
+++ gpu_bdb/benchmark_runner/slurm/spawn-workers.sh
@@ -9,6 +9,10 @@ export INTERFACE="enp97s0f1"
 export BLAZING_ALLOCATOR_MODE="existing"
 export BLAZING_LOGGING_DIRECTORY=/gpu-bdb-data/gpu-bdb/blazing_log
 
+# sleep for 15 seconds to handle cases in which this job spins up workers
+# before the scheduler is ready
+sleep 15
+
 echo $LOGDIR
 echo ls -l $LOGDIR
 bash $GPU_BDB_HOME/gpu_bdb/cluster_configuration/cluster-startup-slurm.sh &
