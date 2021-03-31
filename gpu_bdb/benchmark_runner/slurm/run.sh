8921fcdd33f6a90a2f9e75238e09e2dd64504a81
Ayush Dattagupta <ayushdg95@gmail.com>
2021-03-31 15:45:35 -0700

    Unify scripts for scheduler and worker jobs on slurm

--- gpu_bdb/benchmark_runner/slurm/run.sh
+++ gpu_bdb/benchmark_runner/slurm/run.sh
@@ -16,27 +16,9 @@ rm -rf $HOME/dask-local-directory/*
 srun \
     --account $ACCOUNT \
     --partition $PARTITION \
-    --nodes 1 \
-    --job-name gpubdb-sched \
+    --nodes $WORKER_NODES \
+    --job-name ${ACCOUNT}-gpubdb:run_bench \
     --time 120 \
     --container-mounts $DATA_PATH:$MOUNT_PATH,$HOME:$HOME \
     --container-image=$IMAGE \
-    bash -c "$GPU_BDB_HOME/gpu_bdb/benchmark_runner/slurm/scheduler-client.sh" &
-
-sleep 15
-
-SCHEDULER_JOBID=$(sacct --starttime $(date -d "-30 seconds" +%FT%H:%M:%S) --endtime $(date -d "+1 days" +%F) -n -X --format jobid --name gpubdb-sched)
-
-if [ "$WORKER_NODES" -gt "0" ]
-then
-    srun \
-        --account $ACCOUNT \
-        --partition $PARTITION \
-        --nodes $WORKER_NODES \
-        --job-name gpubdb-workers \
-        --time 120 \
-        --dependency after:$SCHEDULER_JOBID \
-        --container-mounts $DATA_PATH:$MOUNT_PATH,$HOME:$HOME \
-        --container-image $IMAGE \
-        bash -c "$GPU_BDB_HOME/gpu_bdb/benchmark_runner/slurm/spawn-workers.sh"
-fi
+    bash -c "$GPU_BDB_HOME/gpu_bdb/benchmark_runner/slurm/run_bench.sh" &
