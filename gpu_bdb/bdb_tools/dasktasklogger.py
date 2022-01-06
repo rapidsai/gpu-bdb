cbd632ecc21b84f885c5cc843e2dc04d978fe6a8
Kevin German <kevin.german@gmail.com>
2022-01-06 08:52:55 -0800

    unneccesary

--- gpu_bdb/bdb_tools/dasktasklogger.py
+++ gpu_bdb/bdb_tools/dasktasklogger.py
@@ -3,8 +3,6 @@ import os
 import json
 import numpy as np
 
-example={'worker': 'tcp://10.180.4.206:42643', 'status': 'OK', 'nbytes': 28263552, 'thread': 140534840973056, 'type': b'\x80\x04\x95%\x00\x00\x00\x00\x00\x00\x00\x8c\x13cudf.core.dataframe\x94\x8c\tDataFrame\x94\x93\x94.', 'typename': 'cudf.core.dataframe.DataFrame', 'metadata': {}, 'startstops': ({'action': 'transfer', 'start': 1639787413.9825313, 'stop': 1639787413.998216, 'source': 'tcp://10.180.4.206:45115'}, {'action': 'compute', 'start': 1639787413.998873, 'stop': 1639787414.0106611}), 'key': "('drop-duplicates-combine-d121e7e64a9ef70e5616e411e95f2d3e', 1, 8, 0)"}
-
 class DaskTaskLogger():
     key_expr=re.compile( '([\w-]+)-([0-9a-f-]{32,36})' )
     
