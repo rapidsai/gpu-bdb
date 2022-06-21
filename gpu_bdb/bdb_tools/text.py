80e19b18020c630144edfa2f06358efbf3bcdf95
Chris Jarrett <cjarrett@exp02.aselab.nvidia.com>
2022-06-21 14:57:03 -0700

    Remove extra whitespace

--- gpu_bdb/bdb_tools/text.py
+++ gpu_bdb/bdb_tools/text.py
@@ -29,16 +29,16 @@ def create_sentences_from_reviews(
     df, review_column="pr_review_content", end_of_line_char=EOL_CHAR,
 ):
     sentences = df[review_column].str.split(end_of_line_char)
-    
+
     if isinstance(df, cudf.DataFrame):
         out = cudf.DataFrame({"sentence": sentences, "review_idx_global_pos": df.pr_review_sk})
     else:
         out = pd.DataFrame({"sentence": sentences, "review_idx_global_pos": df.pr_review_sk})
-    
+
     out= out.explode("sentence", ignore_index=True)
     out["sentence"] = out.sentence.replace('', np.nan)
     out = out.dropna().reset_index(drop=True)
-        
+
     out["review_idx_global_pos"] = out["review_idx_global_pos"].astype("int32")
     return out
 
@@ -50,10 +50,10 @@ def create_words_from_sentences(
     delimiter=" ",
 ):
     cleaned_sentences = df[sentence_column].str.replace(".", "", regex=False) 
-    
+
     for char in [",", ";", "-", '\"']:
         cleaned_sentences = cleaned_sentences.str.replace(char, "", regex=False) 
-        
+
     normalized_sentences = cleaned_sentences.str.strip() 
     words = normalized_sentences.str.split(delimiter)
 
@@ -61,9 +61,9 @@ def create_words_from_sentences(
         out = cudf.DataFrame({"word": words, "sentence_idx_global_pos": df[global_position_column]})
     else:
         out = pd.DataFrame({"word": words, "sentence_idx_global_pos": df[global_position_column]})
-    
+
     out = out.explode("word", ignore_index=True)
     out["word"] = out.word.replace('', np.nan)
     out = out.dropna().reset_index(drop=True)
-    
+
     return out
